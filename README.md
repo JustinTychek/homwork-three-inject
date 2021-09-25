ad1bc25b546d3acb6392668843b54daf25aa216a
Justin Tychek <jmtychek@iu.edu>
2021-09-25 19:54:35 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# homwork-three-inject
+ project that uses html injection and mvc
---
+++ app/app.js
@@ -0,0 +1,28 @@
+function route() {
+  let hashTag = window.location.hash;
+  let pageID = hashTag.replace("#/", "");
+
+  //takes you to the home page if the url is empty
+  if (pageID == "") {
+    navToPage("home");
+  } else {
+    navToPage(pageID);
+  }
+}
+
+//function that takes you to the page based on the pageID
+function navToPage(pageName) {
+  MODEL.getPageContent(pageName);
+}
+
+function initListeners() {
+  $(window).on("hashchange", route);
+  route();
+}
+
+$(document).ready(function () {
+  initListeners();
+
+  //use when developing to take your directly to the page you want to work on
+  //   navToPage("contact");
+});
